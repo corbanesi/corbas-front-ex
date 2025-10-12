e9700cb9c289bbad91c6e684effc156ef6898d9e
corbanesi <alessandrocorbanesi@gmail.com>
2025-10-12 15:31:30 -0300

    logout

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -22,6 +22,7 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { useNavigate } from "@tanstack/react-router";
 
 export function NavUser({
   user,
@@ -33,6 +34,14 @@ export function NavUser({
   };
 }) {
   const { isMobile } = useSidebar();
+
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    navigate({
+      to: "/login"
+    });
+  }
 
   return (
     <SidebarMenu>
@@ -92,7 +101,7 @@ export function NavUser({
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={handleLogout}>
               <IconLogout />
               Log out
             </DropdownMenuItem>
